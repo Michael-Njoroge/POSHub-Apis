6ac3a061f5f40f4da4432f79945bc786a5ed42c0
Michael-Njoroge <mikethecoder12@gmail.com>
2024-12-22 22:16:03 +0300

    controllers and apis

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -5,18 +5,23 @@ namespace App\Http\Controllers;
 use App\Http\Resources\ProductCategoryResource;
 use App\Http\Resources\ProductResource;
 use App\Http\Resources\ProductStatusResource;
+use App\Http\Resources\UsersResource;
 use App\Http\Resources\WarehouseResource;
+use App\Models\Media;
 use App\Models\ProductCategory;
 use App\Models\Products;
 use App\Models\ProductStatus;
+use App\Models\Rating;
+use App\Models\User;
 use App\Models\Warehouse;
+use CloudinaryLabs\CloudinaryLaravel\Facades\Cloudinary;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Str;
 
 class ProductsController extends Controller
 {
-
+    //Create product category
     public function create_category(Request $request)
     {
         $data = $request->validate([
@@ -27,7 +32,8 @@ class ProductsController extends Controller
         $created_category = ProductCategory::find($category->id);
         return $this->sendResponse(ProductCategoryResource::make($created_category)->response()->getData(true), 'Category created successfully');
     }
-
+    
+    //Create product status
     public function create_product_status(Request $request)
     {
         $data = $request->validate([
@@ -47,17 +53,21 @@ class ProductsController extends Controller
         return $this->sendResponse(ProductStatusResource::make($created_status)->response()->getData(true), 'Status created successfully');
     }
 
+    //Get all product statuses
     public function get_product_statuses(Request $request)
     {
         $statuses = ProductStatus::paginate(25);
         return $this->sendResponse(ProductStatusResource::collection($statuses)->response()->getData(true), 'Statuses retrieved successfully');
     }
 
+    //Get all product categories
     public function get_categories(Request $request)
     {
         $categories = ProductCategory::paginate(25);
         return $this->sendResponse(ProductCategoryResource::collection($categories)->response()->getData(true), 'Categories retrieved successfully');
     }
+
+    //Create product
     public function create_product(Request $request)
     {
         $data = $request->validate([
@@ -74,24 +84,46 @@ class ProductsController extends Controller
             'type' => 'nullable|string',
             'author' => 'nullable|string',
             'description' => 'nullable|string',
-            'image' => 'nullable|image|mimes:jpeg,png,jpg,gif,svg|max:2048',
             'warehouse_quantities' => 'nullable|array',
             'warehouse_quantities.*.warehouse_id' => 'required|uuid|exists:pos_warehouses,id|distinct',
-            'warehouse_quantities.*.quantity' => 'required|integer|min:0'
+            'warehouse_quantities.*.quantity' => 'required|integer|min:0',
+            'color' => 'nullable|array',
+            'color.*' => 'uuid|exists:colors,id',
+            'tags' => 'required|string',
+            'media_ids' => 'nullable|array', 
+            'media_ids.*' => 'uuid|exists:media,id',
         ]);
 
-        if ($request->hasFile('image')) {
-            $image = $request->file('image');
-            $uuid = Str::uuid();
-            $image_name = $uuid . '-' . time() . '.' . $image->getClientOriginalExtension();
-            $image_path = $image->storeAs('product_images', $image_name, 'public');
-            $data['image'] = asset('storage/' . $image_path);
+        // if ($request->hasFile('image')) {
+        //     $image = $request->file('image');
+        //     $uuid = Str::uuid();
+        //     $image_name = $uuid . '-' . time() . '.' . $image->getClientOriginalExtension();
+        //     $image_path = $image->storeAs('product_images', $image_name, 'public');
+        //     $data['image'] = asset('storage/' . $image_path);
+        // }
+
+        $slug = Str::slug($data['title']);
+        $data['slug'] = $slug;
+        // dd($slug);
+        $product = Products::where('slug', $slug)->first();
+
+        if($product){
+            return $this->sendError($error = 'Product with this slug already exists', $code = 403);
         }
+
+        $mediaData = $request->input('media_ids');
+        unset($data['media_ids']);
 
         $warehouse_quantites = $request->input('warehouse_quantities', []);
         unset($data['warehouse_quantities']);
 
         $product = Products::create($data);
+
+        if(!empty($mediaData)) {
+            Media::whereIn('id', $mediaData)
+                ->update(['medially_id' => $product->id, 'medially_type' => Products::class]);
+        }
+
         $warehouses = Warehouse::all();
 
         $total_quantity = 0;
@@ -138,11 +170,11 @@ class ProductsController extends Controller
 
         $product->update(['quantity' => $total_quantity]);
 
-        $created_product = Products::with('category', 'status', 'warehouse_quantities')->find($product->id);
+        $created_product = Products::with('category', 'status', 'warehouse_quantities','media','ratings.user')->find($product->id);
         return $this->sendResponse(ProductResource::make($created_product)->response()->getData(true), 'Product created successfully');
     }
 
-
+    //Get all products
     public function get_products(Request $request)
     {
         try {
@@ -209,6 +241,14 @@ class ProductsController extends Controller
         }
     }
 
+    // Get single product
+    public function get_product(Products $product)
+    {
+        $product->load('media', 'ratings.user');
+        return $this->sendResponse(ProductResource::make($product)->response()->getData(true), 'Product retrieved successfully');
+    }
+
+    //Create warehouse
     public function create_warehouse(Request $request)
     {
         if (!auth()->user()->inGroup('Owner')) {
@@ -226,12 +266,98 @@ class ProductsController extends Controller
         return $this->sendResponse(WarehouseResource::make($created_warehouse)->response()->getData(true), 'Warehouse created successfully');
     }
 
+    //Update product
+    public function update_product(Request $request, Products $product)
+    {
+        $mediaData = $request->input('media_ids');
+        if(!empty($mediaData)){
+            Media::whereIn('id', $mediaData)->update(['medially_id' => $product->id, 'medially_type' => Products::class]);
+        }
+       $requestData = $request->except(['media_ids']);
+        $product->update($requestData);
+        $updatedProduct = Products::with('media','ratings.user')->find($product->id);
+        return $this->sendResponse(ProductResource::make($updatedProduct)->response()->getData(true), 'Product updated successfully');
+    }
+
+    //Delete product
+    public function delete_product(Request $request, Products $product)
+    {
+        $mediaItems = Media::where('medially_id', $product->id)->where('medially_type', Products::class)->get();
+        foreach ($mediaItems as $mediaItem) {
+            try{
+                Cloudinary::destroy($mediaItem->public_id);
+                $mediaItem->delete();
+            }catch(\Exception $e){
+                return response()->json(['error' => $e->getMessage()], 500);
+            }
+        }
+        $product->delete();
+        return $this->sendResponse([], 'Product deleted successfully');
+    }
+
+    //Add to wishlist
+    public function add_to_wishlist(Request $request, Products $product)
+    {
+        $user = auth()->user();
+        if ($user->wishlist()->where('product_id', $product->id)->exists()) {
+            $user->detach('wishlist', $product->id);
+            $user->load('wishlist');
+            return $this->sendResponse([], 'Product removed from wishlist');
+        }else{
+            $user->attach('wishlist', $product->id);    
+            $user->load('wishlist');
+            return $this->sendResponse(UsersResource::make($user)->response()->getData(true), 'Product added to wishlist');
+        }
+    }
+
+    //Get user wishlist
+    public function get_wishlist()
+    {
+        $user = auth()->user();
+        $wishlist = $user->wishlist()->paginate(25);
+        $user->load(['wishlist','ratings.user']);
+        return $this->sendResponse(ProductResource::make($wishlist)->response()->getData(true), 'User wishlist retrieved successfully');
+    }
+
+    //Rate a product
+    public function rate_product(Request $request, Products $product)
+    {
+        $user = auth()->user();
+        $data = $request->validate([
+            'star' => 'required|integer|min:1|max:5',
+            'comment' => 'nullable|string',
+            'product' => 'required|exists:products,id',
+        ]);
+        $existingRating = $product->ratings()->where('user_id', $user->id)->first();
+        if ($existingRating) {
+            $existingRating->update([
+                'star' => $data['star'],
+                'comment' => $data['comment'],
+            ]);
+            $message = 'Rating updated successfully';
+        }else{
+            Rating::create([
+                'user_id' => $user->id,
+                'product_id' => $product->id,
+                'star' => $data['star'],
+                'comment' => $data['comment'],
+            ]);
+            $message = 'Rating created successfully';
+        }
+        $totalRating = $product->ratings()->avg('star');
+        $product->update(['total_ratings' => $totalRating]);
+        $product->load('ratings.user', 'media');
+        return $this->sendResponse(ProductResource::make($product)->response()->getData(true), $message);
+    }
+
+    //Get all warehouses
     public function get_warehouses(Request $request)
     {
         $warehouses = Warehouse::all();
         return $this->sendResponse(WarehouseResource::collection($warehouses)->response()->getData(true), 'Warehouses retrieved successfully');
     }
 
+    //Get warehouse products
     public function get_warehouse_products(Request $request, Warehouse $warehouse)
     {
         $products = $warehouse->products()->paginate(25);
